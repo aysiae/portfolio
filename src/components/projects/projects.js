880b1acd134008201e053e72694dab9e8cfa8c95
Aysia Brown <aysiaebrown@gmail.com>
2021-03-16 14:32:46 -0700

    added header for projects section

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -9,9 +9,9 @@ const placeholder = 'https://images.unsplash.com/photo-1587835053626-f88000149fb
 
 function Projects() {
     const projects = [
-        {title: 'Writrs.', repo: 'https://github.com/aysiae/writing-site', img: Writrs, desc:'A tool for writers made by writers.', tools: 'JavaScript, SCSS, ReactJS, Redux, Firebase', site: ''},
-        {title: 'E-Commerce Store', repo: 'https://github.com/aysiae/storefront', img: placeholder , desc: 'An example of an E-commerice storefront, grabbing inventory from an API.', tools: 'JavaScript, ReactJS, Redux, Materials-UI, RESTful API', site: ''},
-        {title: 'Task Manager', repo:'https://github.com/aysiae/todo', img: placeholder, desc: 'A task manager to assign difficulty and tasks to specific persons.', tools: 'JavaScript, ReactJS, SCSS', site: ''},
+        {title: 'Writrs', repo: 'https://github.com/aysiae/writing-site', img: Writrs, desc:'A tool for writers made by writers.', tools: 'JavaScript, SCSS, ReactJS, Redux, Firebase', site: ''},
+       // {title: 'E-Commerce Store', repo: 'https://github.com/aysiae/storefront', img: placeholder , desc: 'An example of an E-commerice storefront, grabbing inventory from an API.', tools: 'JavaScript, ReactJS, Redux, Materials-UI, RESTful API', site: ''},
+        // {title: 'Task Manager', repo:'https://github.com/aysiae/todo', img: placeholder, desc: 'A task manager to assign difficulty and tasks to specific persons.', tools: 'JavaScript, ReactJS, SCSS', site: ''},
         {title: 'Rescuties', repo:'https://github.com/aysiae/rescuties', img: Rescuties, desc:'Bridging the information gap between prospective pet owners and adoptable shelter pets', tools: 'JavaScript, ReactJS, Redux, Materials-UI, ReactStrap, Firebase', site: 'https://rescuties-401d39.web.app/page/pets'},
         {title: 'Portfolio (this!)', repo:'https://github.com/aysiae/portfolio', img: Portfolio, desc: 'A web developer portfolio site.', tools:'JavaScript, SCSS, ReactJS', site: ''}
     ]
@@ -19,14 +19,15 @@ function Projects() {
     return(
         <>
         <div id='projects'>
+            <h2>Projects</h2>
             <div className='card-container'>
                 {projects.map(project => (
                     <div className='card'>
                         <img src={project.img}/>
-                        <h2>{project.title}</h2>
+                        <h3>{project.title}</h3>
+                        <a href={project.repo}>GitHub Repo</a>
                         <p>{project.desc}</p>
                         <p>Technologies Used: <br/> {project.tools}</p>
-                        <a href={project.repo}>GitHub Repo</a>
                     </div>
                 ))}
             </div>
