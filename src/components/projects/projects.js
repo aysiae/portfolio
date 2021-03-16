187898dab1dcd0db6c08b1d571795e1929f834f3
Aysia Brown <aysiaebrown@gmail.com>
2021-03-16 14:00:44 -0700

    projects section paralax scroll

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -4,14 +4,16 @@ import './projects.scss';
 import Rescuties from '../../assets/rescuties-home.png'
 import Portfolio from '../../assets/portfolio-header.png'
 import Writrs from '../../assets/writrs-char.png'
+const placeholder = 'https://images.unsplash.com/photo-1587835053626-f88000149fbf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80';
+
 
 function Projects() {
     const projects = [
-        {title: 'Writrs.', repo: 'https://github.com/aysiae/writing-site', img: Writrs, desc:'', tools: '', site: ''},
-        {title: 'E-Commerce Store', repo: 'https://github.com/aysiae/storefront', img: '', desc: '', tools: '', site: ''},
-        {title: 'Task Manager', repo:'https://github.com/aysiae/todo', img: '', desc: '', tools: '', site: ''},
-        {title: 'Rescuties', repo:'https://github.com/aysiae/rescuties', img: Rescuties, desc:'', site: 'https://rescuties-401d39.web.app/page/pets'},
-        {title: 'Portfolio (this!)', repo:'https://github.com/aysiae/portfolio', img: Portfolio, desc: '', tools:'', site: ''}
+        {title: 'Writrs.', repo: 'https://github.com/aysiae/writing-site', img: Writrs, desc:'A tool for writers made by writers.', tools: 'JavaScript, SCSS, ReactJS, Redux, Firebase', site: ''},
+        {title: 'E-Commerce Store', repo: 'https://github.com/aysiae/storefront', img: placeholder , desc: 'An example of an E-commerice storefront, grabbing inventory from an API.', tools: 'JavaScript, ReactJS, Redux, Materials-UI, RESTful API', site: ''},
+        {title: 'Task Manager', repo:'https://github.com/aysiae/todo', img: placeholder, desc: 'A task manager to assign difficulty and tasks to specific persons.', tools: 'JavaScript, ReactJS, SCSS', site: ''},
+        {title: 'Rescuties', repo:'https://github.com/aysiae/rescuties', img: Rescuties, desc:'Bridging the information gap between prospective pet owners and adoptable shelter pets', tools: 'JavaScript, ReactJS, Redux, Materials-UI, ReactStrap, Firebase', site: 'https://rescuties-401d39.web.app/page/pets'},
+        {title: 'Portfolio (this!)', repo:'https://github.com/aysiae/portfolio', img: Portfolio, desc: 'A web developer portfolio site.', tools:'JavaScript, SCSS, ReactJS', site: ''}
     ]
 
     return(
@@ -22,6 +24,9 @@ function Projects() {
                     <div className='card'>
                         <img src={project.img}/>
                         <h2>{project.title}</h2>
+                        <p>{project.desc}</p>
+                        <p>Technologies Used: <br/> {project.tools}</p>
+                        <a href={project.repo}>GitHub Repo</a>
                     </div>
                 ))}
             </div>
